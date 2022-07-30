6a65a1f60c90094142d96a7117af4adc0541b5ea
SikinoGaming <sikino.gaming@gmail.com>
2022-07-30 18:58:55 +0200

    Fixed bug were log file contain only the last log line.

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,10 +1,15 @@
 from datetime import datetime
+import os
 
 class Logger:
 
     def __init__(self):
         self.start_date = datetime.now()
-        self.log_file = "MSD_" + str(self.start_date.year) + "-" + str(self.start_date.day) + "-" + str(self.start_date.month) + "_" + str(self.start_date.hour) + ":" + str(self.start_date.minute) + ":" + str(self.start_date.second) + ".log"
+        # Check if log folder exists
+        if not os.path.exists("../logs"):
+            os.makedirs("../logs")
+            print("Created 'log' folder because it didn't exist")
+        self.log_file = "../logs/MSD_" + str(self.start_date.year) + "-" + str(self.start_date.day) + "-" + str(self.start_date.month) + "_" + str(self.start_date.hour) + ":" + str(self.start_date.minute) + ":" + str(self.start_date.second) + ".log"
         with open(self.log_file, "w+") as log_file: log_file.close()
 
 
@@ -12,8 +17,8 @@ class Logger:
         current_date = datetime.now()
         msg = "[" + str(current_date.month) + "/" + str(current_date.day) + "/"  + str(current_date.year) + "-" + str(current_date.hour) + ":" + str(current_date.minute) + ":" + str(current_date.second) + "." + str(current_date.microsecond) + "]" + "[" + window_name + "] " + str(msg)
         
-        with open(self.log_file, "w+") as log_file:
-            log_file.write(msg + "\n")
-            log_file.close()
+        with open(self.log_file, "a") as file:
+            file.write(msg + "\n")
+            file.close()
         
         print(msg)
