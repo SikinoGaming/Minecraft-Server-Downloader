b216023f93034fce0751d31def3c9086ca916a74
SikinoGaming <sikino.gaming@gmail.com>
2022-08-10 17:51:12 +0200

    Added import download verification

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-import tkinter
+try:
+    import tkinter
+    import pySmartDL
+    import psutil
+except ImportError as e:
+    print("Can't import library " + str(e.name) + ", try \"pip install -r ../gui-requirements.txt\" and retry")
+    exit(1)
+
 from windows.download import DownloadWindow
 from utils.logger import Logger
 
-logger = Logger()
 
-window = tkinter.Tk()
-DownloadWindow(window, logger)
-window.mainloop()
+
+if __name__ == "__main__":
+    logger = Logger()
+    
+    window = tkinter.Tk()
+    DownloadWindow(window, logger)
+    window.mainloop()
